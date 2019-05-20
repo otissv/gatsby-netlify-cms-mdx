52626802a2f3d976a520780a9191660c55a8dcad
Otis Virginie <otissv@gmail.com>
2019-05-20 19:09:25 +0100

    added themed

--- src/features/App/AppProvider.js
+++ src/features/App/AppProvider.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { AppContext } from "./AppContext"
+import { ThemeProvider } from "redesign-system/lib/ThemeContext"
 
 export function AppProvider({ children }) {
   const context = {
     movies: ["Avengers", "Starwars", "Goonies"],
   }
 
-  return <AppContext.Provider value={context}>{children}</AppContext.Provider>
+  return (
+    <ThemeProvider>
+      <AppContext.Provider value={context}>{children}</AppContext.Provider>
+    </ThemeProvider>
+  )
 }
